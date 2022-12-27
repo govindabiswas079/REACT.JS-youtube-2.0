f07b5b171758658923b23b2b2373a162d3fa8533
Anjali Korekar <govindbiswas079@gmail.com>
2022-12-27 19:44:44 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '2a9f883453msha39c0a2a7519df6p1cf416jsn71a3bce9a381',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
+
+
+// https://rapidapi.com/ytdlfree/api/youtube-v31
